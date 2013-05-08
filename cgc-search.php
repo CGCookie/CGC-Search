7d5544be6b0a854249d3559a341b2a76c345083d
Pippin Williamson <pippin@pippinsplugins.com>
2013-05-08 10:59:38 -0500

    updated search form

--- cgc-search.php
+++ cgc-search.php
@@ -12,25 +12,56 @@ class CGC_Search_Form {
 
 		// Filters
 		add_filter( 'get_search_form', array( $this, 'searchform' ) );
+		add_filter( 'the_search_query', array( $this, 'tweak_search' ) );
 
 		// Actions
-
+		add_action( 'template_redirect', array( $this, 'search_template' ) );
 
 	}
 
 
 	// Override the default get_search_form() call
 	public function searchform( $form ) {
-		$form = '<form role="search" method="get" id="searchform" action="' . home_url( '/' ) . '" >
-		<div><label class="screen-reader-text" for="s">' . __('Search for:') . '</label>
-		<input type="text" value="' . get_search_query() . '" name="s" id="s" />
-		<input type="submit" id="searchsubmit" value="'. esc_attr__('Search') .'" />
-		</div>
-		</form>';
 
-		return $form;
+		ob_start(); ?>
+		<form role="search" method="get" id="searchform" action="<?php echo home_url(); ?>">
+			<label class="screen-reader-text" for="searchinput"></label>
+			<input type="text" name="s" id="searchinput" value="<?php echo get_search_query(); ?>"/>
+			<select name="type" id="cgc-search-type">
+				<option value="tutorials">Tutorials</option>
+				<option value="members">Members</option>
+			</select>
+			<button type="submit" id="searchsubmit"><i class="icon-search"></i></button>
+		</form>
+		<?php
+		return ob_get_clean();
 	}
 
+
+	// Set categories, tags, post types, etc
+	public function tweak_search( $query ) {
+
+		return $query;
+	}
+
+
+	// Redirect to custom template for user search
+	public function search_template() {
+
+		if( empty( $_GET['s'] ) )
+			return;
+
+		if( empty( $_GET['type'] ) )
+			return;
+
+		if( 'members' != $_GET['type'] )
+			return;
+
+		$templates = array( 'search-members.php' );
+
+		locate_template( $templates, true, true );
+		exit;
+	}
 }
 
 // Instantiate the class
