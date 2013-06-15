3e119dae6015f5bb70ab16497b188b571e966c1c
Pippin Williamson <pippin@pippinsplugins.com>
2013-06-15 14:59:28 -0500

    check for is_search()

--- cgc-search.php
+++ cgc-search.php
@@ -228,8 +228,8 @@ class CGC_Search_Form {
 	// Set categories, tags, post s_types, etc
 	public function tweak_search( $query ) {
 
-		if( ! is_object( $query ) )
-			return;
+		if( ! is_search() )
+			return $query;
 
 		if( $query->is_main_query() && $query->is_search() && isset( $_GET['cgc-search'] ) ) {
 
